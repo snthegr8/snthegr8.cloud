db6a57838a631406bcd2eb8eb560fe789fc9927a
Somtochukwu <leroynnalue65@gmail.com>
2026-08-22 22:16:25 +0100

    chore: better intro of me

--- src/data/home.ts
+++ src/data/home.ts
@@ -9,9 +9,10 @@ export const home = {
     alt: 'Somtochukwu Nnalue',
   },
   paragraphs: [
-    "Hi there,",
-    "I'm Somtochukwu Nnalue, Leroy to most people. Software engineer based in Lagos, Nigeria. Building software at Anamen.",
-  ],
+    "Hi there, greetings to you!",
+    "I'm Somtochukwu Nnalue, Leroy to most people, a software developer based in Lagos, Nigeria. I've been building cool apps for over 2 years, and while most of my builds are offline or propietary value offering software, I have two exciting projects cooking for you! During the day, you'd find me at my favorite place, Anamen. By night, as a Dark Knight, I am working on a platform that will offer creators a great deal of value.",
+    "When I'm not in front of Kleos, my MacBook, you'd probably find me on my bicycle riding without a worry in mind, or trying to finish up The Mentalist series.",
+    "I don't have a handful of friends, so if we share similar interests, or you have a tip for me, want to hang out, or have something valuable to build, feel free to email me below 👇"],
   links: [
     { label: 'Experience', href: '/experience' },
     { label: 'Github', href: 'https://github.com/snthegr8' },
